52c122555607a7dab781a260e582fabb1abfc4a0
immanuelvatta <immanuelvatta@gmail.com>
2023-07-15 00:26:28 -0500

    moved some files

--- flask_mysql/crud/demo/flask_app/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask, render_template, session, request, redirect
-
-app = Flask(__name__)
-
-app.secret_key = "do not forget to add secret key"
--- flask_mysql/crud/demo/server.py
+++
@@ -1,6 +0,0 @@
-from flask_app import app
-from flask_app.controllers import controller_dealer, controller_car, controller_routes
-
-
-if __name__=="__main__":
-    app.run(debug=True)
